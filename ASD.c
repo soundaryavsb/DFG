73cc9d68bb7be8efc65905c840b17cf2c7e73770
soundaryavsb <31506228+soundaryavsb@users.noreply.github.com>
2018-02-06 14:42:27 +0530

    Update ASD.c

--- ASD.c
+++ ASD.c
@@ -2,14 +2,14 @@
 
 int main(void) {
 	int fp,sp,gcs;
-	char ch;
+	int ch;
 printf("Enter first price,second price,Amount given by student customer\n");
 scanf("%d%d%d",&fp,&sp,&gcs);
-printf("You want recept give y \n you dont want recept give n \n ");
-scanf("%c",&ch);
+printf("You want recept give 1 \n you dont want recept give 2 \n ");
+scanf("%d",&ch);
 switch(ch)
 {
-	case 'y':
+	case 1:
 	{
 printf("First item price:RS %d\n",fp);
 printf("Second item price:RS %d\n",sp);
@@ -21,7 +21,9 @@ printf("Amount returned by shop keeper:%d\n",gcs-(fp+sp));
 printf("*************Thank you visit again***************");
 }
 break;
-case 'n':
+case 2:
 printf("no");
 break;
+}
+return 0;
 }
